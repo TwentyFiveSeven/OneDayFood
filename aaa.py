64c8e33b4ab99e8ec0d11acd7f68fd3745c4a368
JinGu <xortm854@naver.com>
2020-06-21 16:16:43 +0900

    change code

--- aaa.py
+++ aaa.py
@@ -3,6 +3,9 @@ from konlpy.utils import pprint
 from konlpy.tag import Okt
 kkma = Kkma()
 okt = Okt()
-p = okt.nouns(u'페퍼로니피자')
-
-print(p)
+inputD = '곤드레 간장게장정식'
+p = okt.pos(inputD)
+p2 = okt.nouns(inputD)
+print('입력 문자 :',inputD)
+print('형태소 분석  :',p)
+print('명사구 추출  :',p2)
